d1fa04f17bcfbd0676b553e2b9516c29de637593
OWK50GA <okoriewifrid.20201206203@futo.edu.ng>
2025-07-22 02:01:35 +0100

    emptied master branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import { RegistrationForm } from "@/components/RegistrationForm";
 import Image from "next/image";
 
 export default function Home() {
   return (
     // flex min-h-screen flex-col items-center justify-between p-24
-    <main className="min-h-screen bg-slate-100">
-      <RegistrationForm />
+    <main className="min-h-screen bg-black grid place-items-center text-white">
+      Starting Page
     </main>
   );
 }
