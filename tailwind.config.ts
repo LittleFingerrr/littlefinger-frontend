0dba9679d32dc80cb1acce23ca202231318f3b68
DioChuks <diochuks65@gmail.com>
2025-07-23 07:39:56 +0100

    feat: add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,7 +68,9 @@ const config: Config = {
   				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
   				border: 'hsl(var(--sidebar-border))',
   				ring: 'hsl(var(--sidebar-ring))'
-  			}
+  			},
+			'regal-black': '#070602',
+			'regal-gold': '#967623',
   		}
   	}
   },
